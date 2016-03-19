1145c857e29f1872ee63f9c4f5b3906a40bc7cf3
Pegcastro <pagoncastro@gmail.com>
2016-03-19 16:40:43 -0430

    Ya funciona el fondo

--- psudoku.py
+++ psudoku.py
@@ -1,26 +1,20 @@
 
 
 # Librerias
-import pygame, sys
-from pygame.locals import *	
+import pygame
+import sys
+from pygame.locals import *
 
 # Colores
-blanco = (255, 255, 255)
-negro = (0, 0, 0)
+blanco = (255,255,255)
+negro = (0,0,0)
+gris = (84,84,84)
+
+
 
 # Funciones
 
-# Dibuja las lineas que separan los numeros, las regiones del tablero y el fondo
-# de la pantalla
-def dibuja_tablero (color1, color2):
-	# Crea el fondo blanco del tablero
-	DISPLAYSURF = pygame.display.fill(color1)
-
-	# Crea las lineas divisorias de los numeros
-	for x in range(0, 800, 30):
-		pygame.draw.line(DISPLAYSURF, color2, (x,0), (x,600))
-	# Crea las lineas divisorias de las regiones y delimitadoras del tablero
-
+#def Menu():
 
 
 ################################################################################
@@ -31,11 +25,16 @@ def dibuja_tablero (color1, color2):
 # Inicializa PyGame
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((800, 600))
+# Fuentes
+fuente = pygame.font.SysFont("monospace",15)
+
+# Crea la pantalla
 pygame.display.set_caption('Sudoku Chevere')
-while True: # main game loop
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
-			pygame.display.update()
+fondo = pygame.display.set_mode((800, 600))
+fondo.fill(blanco)
+
+while True:
+	event = pygame.event.poll()
+	if event.type == QUIT:
+		break
+	pygame.display.update()
